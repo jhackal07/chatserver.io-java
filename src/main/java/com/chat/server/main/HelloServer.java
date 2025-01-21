cd4430066beacfe61505024a1a588890501f19b7
Joey Al <52604751+jhackal07@users.noreply.github.com>
2025-01-22 01:00:39 +0800

    Fix import for HelloServer

--- src/main/java/com/chat/server/main/HelloServer.java
+++ src/main/java/com/chat/server/main/HelloServer.java
@@ -2,7 +2,7 @@ package com.chat.server.main;
 
 import java.io.IOException;
 
-import com.chat.server.impl.vo.ChatServer;
+import com.chat.server.impl.ChatServer;
 
 public class HelloServer {
     public static void main(String[] args) throws IOException, InterruptedException {
